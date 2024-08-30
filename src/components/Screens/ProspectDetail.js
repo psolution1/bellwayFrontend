78d563688e23349ec3ad7736df65d828ddd14152
S-singh1990 <148949507+S-singh1990@users.noreply.github.com>
2024-08-30 11:41:35 +0530

    update

--- src/components/Screens/ProspectDetail.js
+++ src/components/Screens/ProspectDetail.js
@@ -65,13 +65,10 @@ function ProspectDetail() {
           // Trigger the autoCall action
           document.getElementById("phone").click();
           // document.querySelector("#phone").click();
-          
           setData({
-            remark: "",
-            schedule: null,
+            ...data,
             callInitiatedTime: new Date(),
           });
-          setSelectDisposition("Lead Generation");
         }
         // Decrement the timer
         return prev - 1;
@@ -142,8 +139,7 @@ function ProspectDetail() {
                       <div className="tab-content">
                         <div className="tab-panel">
                           <div className="row">
-                            
-                            <div className="col-12 col-lg-6 col-md-6 col-xl-6">
+                            <div className="col-12 col-lg-4 col-md-4 col-xl-4">
                               <div className="jss1455 jss148">
                                 <div className="jss1490 jss1579">
                                   <div className="prospect-wrap">
@@ -219,9 +215,9 @@ function ProspectDetail() {
                                             <Link
                                               id="phone"
                                               to={`tel:${prospect?.phone}`}
-                                              // onClick={(e) =>
-                                              //   startCall(e, prospect?.phone)
-                                              // }
+                                              onClick={(e) =>
+                                                startCall(e, prospect?.phone)
+                                              }
                                             >
                                               {prospect?.phone}
                                             </Link>
@@ -230,16 +226,16 @@ function ProspectDetail() {
                                                                                         <i className="fa fa-whatsapp"></i>
                                                                                     </a> */}
                                           </em>
-                                          <Link
+                                          {/* <Link
                                             id="phonex"
                                             // className="whatsapp"
-                                            to={`https://wa.me/${prospect?.phone}`}
-                                            // onClick={(e) =>
-                                            //   startCall(e, prospect?.phone)
-                                            // }
+                                            to={`whatsapp://send?abid=${prospect?.phone}&text=Hello%2C%20World!`}
+                                            onClick={(e) =>
+                                              startCall(e, prospect?.phone)
+                                            }
                                           >
-                                            <i className="fab fa-whatsapp"></i>
-                                          </Link>
+                                            <i className="fa fa-whatsapp"></i>
+                                          </Link> */}
                                         </div>
                                       </div>
                                       <div className="row">
@@ -278,7 +274,7 @@ function ProspectDetail() {
                                       </div>
                                     </div>
                                   </div>
-                                  <div className="row pt-2 d-none">
+                                  <div className="row pt-2">
                                     <div className="col-12 col-xl-12">
                                       <div
                                         className="accordion accordion-flush"
@@ -378,7 +374,7 @@ function ProspectDetail() {
                               <div className="jss187 jss191" />
                             </div>
 
-                            <div className="col-12 col-lg-6 col-md-6 col-xl-6">
+                            <div className="col-12 col-lg-4 col-md-4 col-xl-4">
                               <div className="jss1455 jss148">
                                 <div className="jss1490 jss1579">
                                   <div className="prospect-wrap">
@@ -444,7 +440,7 @@ function ProspectDetail() {
                                               </li>
                                             ))}
                                             {selectDisposition ==
-                                            "Not Interested" || selectDisposition == "Lead Generation" ? null : (
+                                            "Not Interested" ? null : (
                                               <li>
                                                 <input
                                                   type="datetime-local"
@@ -516,7 +512,7 @@ function ProspectDetail() {
                               </div>
                             </div>
 
-                            <div className="col-12 col-lg-4 col-md-4 col-xl-4 d-none">
+                            <div className="col-12 col-lg-4 col-md-4 col-xl-4">
                               <div className="jss1455 jss148">
                                 <div className="jss1490 jss1579">
                                   <div className="prospect-wrap">
